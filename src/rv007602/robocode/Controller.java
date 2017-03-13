7c3569bc056e68db47ecd7a415696fc126e00689
Dan Hughes <danhughes.dh+github@gmail.com>
2017-03-13 21:45:02 +0000

    Change to elitism model

--- src/rv007602/robocode/Controller.java
+++ src/rv007602/robocode/Controller.java
@@ -4,24 +4,21 @@ public class Controller {
 
 	public static void main(String[] args) throws Exception {
 
-		int generations = 30;
-		int survivors = 8;
-		int populationSize = 14;
-		int roundsPerBattle = 2;
+		int generations = 50;
+		int elites = 8;
+		int populationSize = 30;
+		int roundsPerBattle = 3;
 		float mutationRate = 0.2f;
 		float crossoverRate = 0.5f;
-		String[] enemies = {"sample.SpinBot", "sample.SpinBot"};
+		String[] enemies = {"sample.SpinBot", "sample.SpinBot", "sample.SpinBot", "sample.SpinBot", "sample.SpinBot"};
 
 		Fitness.setEnemies(enemies);
-
 		Fitness.setRoundsPerBattle(roundsPerBattle);
-
 		Fitness.setVisible(false);
-
 		Fitness.initialize();
 
 		Population population = new Population(populationSize);
-		Fitness.analyze(population, 0, -1);
+		Fitness.analyze(population, 0, generations);
 
 		Output summary = new Output("summary.csv");
 		Output bulk = new Output("bulk.csv");
@@ -38,19 +35,19 @@ public class Controller {
 		int i = 0;
 		while (i++ < generations) {
 			System.out.println("== Generation " + i);
-			Population parents = population.select(survivors);
-			Population children = parents.crossover(crossoverRate);
-			children.mutate(mutationRate);
 
-			population = parents;
-			population.add(children);
+			Population offspring = population
+				.select(populationSize - elites)
+				.crossover(crossoverRate)
+				.mutate(mutationRate);
+
+			population.cullTo(elites);
+			population.add(offspring);
 
 			Fitness.analyze(population, i, generations);
 
 			summary.addRow(population);
 			bulk.addRow(population);
-
-			population.cullTo(populationSize);
 		}
 
 		Fitness.cleanUp();
